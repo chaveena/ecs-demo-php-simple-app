b3f20501dd631f18ce5be77b4a9968c5e0799163
Chandran <chaveena@60f81dca2e96.ant.amazon.com>
2018-10-30 16:09:15 -0400

    personalize it a lil

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
+                <h1>Playlistify Demo</h1>
                 <h2>Yeehaw!</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <p>This is running on ECS! And changes are updated through CodePipeline.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
